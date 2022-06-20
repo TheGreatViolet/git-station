77d06a780617aacd0cd93a205e6afa827fc49978
Violet Hayes <ampc11234@gmail.com>
2022-06-20 10:34:06 -0700

    Created basic auth. for GitHub

--- pages/Welcome.tsx
+++ pages/Welcome.tsx
@@ -1,7 +1,38 @@
+import { DiscoveryDocument, makeRedirectUri, useAuthRequest } from "expo-auth-session";
 import { Button, Container, Heading, NativeBaseProvider, Row, Stack, Text } from "native-base";
+import { useEffect } from "react";
 import Icon from 'react-native-vector-icons/Octicons';
 
-const Welcome = () => {
+const Welcome: React.FC = () => {
+  const discoveryDev: DiscoveryDocument = {
+    authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+    tokenEndpoint: 'https://github.com/login/oauth/access_token',
+    revocationEndpoint: 'https://github.com/settings/connections/applications/d9c046eee0179d3eb3bb'
+  };
+
+  const discoveryProd: DiscoveryDocument = {
+    authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+    tokenEndpoint: 'https://github.com/login/oauth/access_token',
+    revocationEndpoint: 'https://github.com/settings/connections/applications/deac60d4cbe65ec0106e'
+  }
+
+  const [req, res, propmtAsync] = useAuthRequest({
+    clientId: __DEV__ ? 'd9c046eee0179d3eb3bb' : 'deac60d4cbe65ec0106e',
+    scopes: ['identity'],
+    redirectUri: makeRedirectUri({})
+  }, __DEV__ ? discoveryDev : discoveryProd);
+
+  const authGithub = async () => {
+    propmtAsync();
+  }
+
+  useEffect(() => {
+    console.log(req?.url);
+    if (res) {
+      console.log(res);
+    }
+  }, [res]);
+
   return (
     <NativeBaseProvider>
       <Container style={{
@@ -13,7 +44,7 @@ const Welcome = () => {
           <Heading>Welcome to GitStation</Heading>
           <Text alignSelf='center'>A better GitHub client for mobile</Text>
 
-          <Button mt='2'>
+          <Button mt='2' onTouchEnd={authGithub} disabled={!req}>
             <Row>
               <Icon name="mark-github" size={20} />
               <Text ml={2}>Sign in with GitHub</Text>
